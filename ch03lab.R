1dac5cf5849b3865ab8185fb52827cf0f3b85b55
Hisato Kato <hisakato@jp.ibm.com>
2018-07-15 18:11:54 +0900

    completed

--- ch03lab.R
+++ ch03lab.R
@@ -59,4 +59,46 @@ summary(lm.fit1)
 ## p. 115
 lm.fit1 = update(lm.fit, ~.-age)
 
+## 3.6.4 Interaction Terms
+summary(lm(medv~lstat*age, data=Boston))
 
+## 3.6.5 Non-linear Transformations of the Predictions
+lm.fit2=lm(medv~lstat+I(lstat^2))
+summary(lm.fit2)
+
+## p. 116
+lm.fit=lm(medv~lstat)
+anova(lm.fit, lm.fit2)
+
+par(mfrow=c(2,2))
+plot(lm.fit2)
+
+## p. 117
+lm.fit5=lm(medv~poly(lstat, 5))
+summary(lm.fit5)
+
+summary(lm(medv~log(rm), data=Boston))
+
+## 3.6.6 Qualitative Predictors
+fix(Carseats)
+names(Carseats)
+
+## p. 118
+lm.fit=lm(Sales~.+Income:Advertising + Price:Age, data = Carseats)
+summary(lm.fit)
+
+attach(Carseats)
+contrasts(ShelveLoc)
+
+## p. 119
+## 3.6.7 Writing Functions
+LoadLibraries
+LoadLibraries()
+LoadLibraries = function () {
+  library(ISLR)
+  library(MASS)
+  print("The libraries have been loaded.")
+}
+
+LoadLibraries
+LoadLibraries()
